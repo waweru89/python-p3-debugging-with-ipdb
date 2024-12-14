53fa8ae480acf0a4c3c9e04a41693f8a2a56b763
MBURU <wawerumburu157@gmail.com>
2024-12-14 23:53:15 +0300

    changes

--- lib/ipdb_is_awesome.py
+++ lib/ipdb_is_awesome.py
@@ -6,9 +6,13 @@ def tracing_the_function():
     inside_the_function = "We're inside the function"
     print(inside_the_function)
     print("We're about to stop because of ipdb!")
+
+   
     ipdb.set_trace()
+
     this_variable_hasnt_been_interpreted_yet = \
         "The program froze before it could read me!"
     print(this_variable_hasnt_been_interpreted_yet)
+
 
 tracing_the_function()
--- lib/testing/ipdb_debugging_test.py
+++ lib/testing/ipdb_debugging_test.py
@@ -3,8 +3,8 @@
 from ipdb_debugging import plus_two
 
 class TestIpdbDebugging:
-    '''ipdb_debugging.py'''
-    
+    '''Tests for ipdb_debugging.py'''
+
     def test_adds_two(self):
-        '''adds_two() adds 2 to input arg and returns sum.'''
-        assert(plus_two(3) == 5)
+        '''adds_two() adds 2 to the input arg and returns sum.'''
+        assert plus_two(3) == 5
